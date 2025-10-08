96e14a5bde970238df9a867720ee4f6124e05503
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-10-09 04:17:53 +0600

    update title in index.html and implement routing in main.tsx and router.tsx

---
+++ src/router.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-red-500">Hello World</div>,
+  },
+]);
