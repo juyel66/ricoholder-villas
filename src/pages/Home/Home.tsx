ccb25cfef0f4f2d0a89ddef0132d402f2d592d7f
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-10-10 00:41:26 +0600

    update: update home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
+import FilterSystem from "@/shared/FilterSystem";
 import Banner from "./Component/Banner";
+import SignatureCardContainer from "./Component/SignatureCardContainer";
 
 
 const Home = () => {
     return (
         <div>
            <Banner />
+           <FilterSystem />
+           <SignatureCardContainer />
            
             
         </div>
