25bae66aa41dc6377f0ddc1ec7dbd569c9059868
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-10-14 05:38:32 +0600

    feat(VideoExperience): update video sources and dimensions for better display

--- src/pages/Rents/VideoExperience.tsx
+++ src/pages/Rents/VideoExperience.tsx
@@ -4,11 +4,11 @@ const VideoExperience = () => {
     return (
         <div className="mt-10">
            <div className="lg:flex hidden">
-           <iframe width="860" height="415" src="https://www.youtube.com/embed/Pq2uwssaFRo?si=G6YuGapMarPVMmYM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+           <iframe width="860" height="715" src="https://www.youtube.com/embed/1dxrO79dbCg?si=i6rNvszkd1ffSQQ8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
            </div>
 
            <div className="flex lg:hidden">
-             <iframe width="860" height="315" src="https://www.youtube.com/embed/ZOnJ6isHYNM?si=utvw6hBRbizK60ay" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> 
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/1dxrO79dbCg?si=i6rNvszkd1ffSQQ8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
            </div>
 
             
