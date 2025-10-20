841dfacb299043610cd04cb6cbd2fdfb900cb370
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-10-21 03:38:07 +0600

    feat(GuestSections): update the guestSections

--- src/pages/Home/Component/GuestSections.tsx
+++ src/pages/Home/Component/GuestSections.tsx
@@ -4,8 +4,8 @@ import GuestSlider from "./GuestSlider";
 const GuestSections = () => {
     return (
         <div>
-            <div className="text-center items-center mt-12 ">
-                <h1 className="text-4xl font-semibold ">Guest Perspectives on Paradise Perfected</h1>
+            <div className="text-center items-center mt-16  ">
+                <h1 className="text-4xl font-semibold  mb-5">Guest Perspectives on Paradise Perfected</h1>
             <p>Whispers of Elegance from Those Who've Experienced the Extraordinary</p>
             </div>
 
