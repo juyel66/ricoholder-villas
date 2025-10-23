c17d68f63d33d46c8d4e49b6965a6f02804624b5
juyel66 <mdjuyelrana.com.bd1@gmail..com>
2025-10-23 17:06:12 +0600

    feat(SalesDetails): SalesDetails page added

---
+++ src/pages/Sales/SalesDetails.tsx
@@ -0,0 +1,22 @@
+import RentsDetailsBanner from "../Rents/RentsDetailsBanner";
+import SpotlightDetails from "../Rents/SpotlingtDetails";
+import SalesIMageGallerySections from "./SalesImageGallerySections";
+
+
+
+
+
+const SalesDetails = () => {
+    return (
+        <div>
+            <RentsDetailsBanner />
+            <SpotlightDetails />
+            <SalesIMageGallerySections />
+            
+            
+            
+        </div>
+    );
+};
+
+export default SalesDetails;
