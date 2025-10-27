f51faf94be83b78b92f6d0374e07de5d91b6ea88
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-10-27 06:40:55 +0600

    feat(VideoExperience): Video experience page update

--- src/pages/Rents/VideoExperience.tsx
+++ src/pages/Rents/VideoExperience.tsx
@@ -2,9 +2,11 @@
 
 const VideoExperience = () => {
     return (
-        <div className="mt-10">
+       <div>
+        <div className="text-4xl font-semibold text-gray-900 mb-8 mt-15 items-center">The Video Experience</div>
+         <div className="mt-10">
            <div className="lg:flex hidden">
-           <iframe width="860" height="715" src="https://www.youtube.com/embed/1dxrO79dbCg?si=i6rNvszkd1ffSQQ8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+           <iframe width="860" height="515" src="https://www.youtube.com/embed/1dxrO79dbCg?si=i6rNvszkd1ffSQQ8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
            </div>
 
            <div className="flex lg:hidden">
@@ -13,6 +15,7 @@ const VideoExperience = () => {
 
             
         </div>
+       </div>
     );
 };
 
