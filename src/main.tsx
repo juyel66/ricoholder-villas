3a2b8d70c26e0b5024e94fd9f9cd970c01a721ce
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-11-09 23:59:10 +0600

    feat: Add axios dependency and remove unused Redux auth slice and store

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,3 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-
-// import { router } from "./router.tsx";
-// import { RouterProvider } from "react-router";
-
-// createRoot(document.getElementById("root")!).render(
-//   <StrictMode>
-//     <RouterProvider router={router} />
-//   </StrictMode>
-// );
-
-
-
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -21,14 +5,30 @@ import "./index.css";
 import { router } from "./router.tsx";
 import { RouterProvider } from "react-router";
 
-import { Provider } from "react-redux";
-import { store } from "./Redux/store/store";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
   </StrictMode>
 );
+
+
+
+
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+
+// import { router } from "./router.tsx";
+// import { RouterProvider } from "react-router";
+
+// import { Provider } from "react-redux";
+// import { store } from "./Redux/store/store";
+
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+
+//     <Provider store={store}>
+//       <RouterProvider router={router} />
+//     </Provider>
+//   </StrictMode>
+// );
