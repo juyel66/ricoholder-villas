f5773ec71735e084344da74ccdbcd899bff648f4
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-11-12 06:12:07 +0600

    feat: Add spacing between components in RentsDetails for improved layout

--- src/pages/Rents/RentsDetails.tsx
+++ src/pages/Rents/RentsDetails.tsx
@@ -12,6 +12,7 @@ const RentsDetails = () => {
             <RentsDetailsBanner />
             <SpotlightDetails />
             <ImageGallerySections />
+        
             
             
             
